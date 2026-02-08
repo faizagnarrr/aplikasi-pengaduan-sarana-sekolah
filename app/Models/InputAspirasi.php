3c164bbd775719b36480aee43cda7dcec8ef50d9
Faiz Agnar <faizagnar552@gmail.com>
2026-02-08 07:27:06 +0700

    Models fixes

--- app/Models/InputAspirasi.php
+++ app/Models/InputAspirasi.php
@@ -9,7 +9,7 @@ class InputAspirasi extends Model
     protected $table = 'input_aspirasi';
     protected $primaryKey = 'id_pelaporan';
     public $incrementing = true;
-
+    
     protected $fillable = [
         'id_pelaporan',
         'nis',
@@ -18,16 +18,25 @@ class InputAspirasi extends Model
         'ket',
     ];
 
+    /**
+     * Relasi ke Siswa melalui NIS
+     */
     public function siswa()
     {
         return $this->belongsTo(Siswa::class, 'nis', 'nis');
     }
-    
+
+    /**
+     * Relasi ke Kategori
+     */
     public function kategori()
     {
         return $this->belongsTo(Kategori::class, 'id_kategori', 'id_kategori');
     }
-    
+
+    /**
+     * Relasi ke Aspirasi
+     */
     public function aspirasi()
     {
         return $this->hasOne(Aspirasi::class, 'id_pelaporan', 'id_pelaporan');
